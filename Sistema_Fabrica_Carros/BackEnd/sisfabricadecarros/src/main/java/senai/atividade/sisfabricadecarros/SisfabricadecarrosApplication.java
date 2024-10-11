f28f92359d9dd35f7e79acbf286692c77d9ae1d2
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-10-10 21:50:46 -0300

    att

---
+++ Sistema_Fabrica_Carros/BackEnd/sisfabricadecarros/src/main/java/senai/atividade/sisfabricadecarros/SisfabricadecarrosApplication.java
@@ -0,0 +1,13 @@
+package senai.atividade.sisfabricadecarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisfabricadecarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisfabricadecarrosApplication.class, args);
+	}
+
+}
---
+++ Sistema_Fabrica_Carros/BackEnd/sisfabricadecarros/src/test/java/senai/atividade/sisfabricadecarros/SisfabricadecarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.atividade.sisfabricadecarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisfabricadecarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
